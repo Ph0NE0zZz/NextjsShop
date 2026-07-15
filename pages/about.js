439e735414da85b30f302ac0929e0d7f5f1ec133
Phone <Hattakorn.1238@gmail.com>
2026-07-15 10:31:33 +0700

    component styles

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
+import styles from "@/styles/About.module.css"
+import Image from "next/image";
 export default function About() {
   return (
     <>
-        <h1>About Shop</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>เกี่ยวกับเรา</h1>
+      <Image src="/about.svg" width={400} height={400} alt="logo" />
+      <p>Welcome To My Shop</p>
+    </div>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import styles from"@/styles/Home.module.css"
+import Link from "next/link"
+import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Home Page1</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Home Page1</h1>
+      <Image src="/shopping.svg" width={400} height={400} alt="logo" />
+      <p>Welcome To My Shop</p>
+      <Link href="/products" className={styles.btn}>All Item</Link>
+    </div>
     </>
   );
 }
