9282694d2407f0a015541510e7b11d46692d1510
Phone <Hattakorn.1238@gmail.com>
2026-08-14 00:44:02 +0700

    getStaticPath

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,9 +1,33 @@
-import { useRouter } from "next/router"
-export default function ProductsDetail (){
-    const router = useRouter()
-    const {id} = router.query
-    return(
-        <h1> รหัสสินค้า : {id}</h1>
-    )
+export async function getStaticPaths() {
+    const res = await fetch("https://dummyjson.com/products?limit=12")
+    const data = await res.json()
+    const paths = data.products.map((item) => {
+        return {
+            params: { id: String(item.id) }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
 
+export async function getStaticProps({ params }) {
+    const id = params.id
+    const res = await fetch("https://dummyjson.com/products/" + id)
+    const data = await res.json()
+    return {
+        props: { product: data }
+    }
+}
+
+export default function ProductsDetail({ product }) {
+    return (
+        <>
+            <h1>รหัสสินค้า : {product.id}</h1>
+            <p>ชื่อสินค้า : {product.title}</p>
+            <p>ราคา : {product.price}</p>
+
+        </>
+    )
 }
